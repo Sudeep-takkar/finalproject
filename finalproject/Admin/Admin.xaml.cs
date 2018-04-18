822221236ceee8533a9c832190f933bc69206586
Sudeep Takkar <sudeep.takkar@yahoo.com>
2018-04-18 18:23:13 -0400

    intialize

---
+++ finalproject/Admin/Admin.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace finalproject.Admin
+{
+    /// <summary>
+    /// Interaction logic for Admin.xaml
+    /// </summary>
+    public partial class Admin : Window
+    {
+        DataSet dataSet = new DataSet();
+        public Admin()
+        {
+            InitializeComponent();
+            GetUsersDetails();
+        }
+
+        public void GetUsersDetails()
+        {
+            try
+            {
+                //EnumerableRowCollection<DataRow> query;
+                if (File.Exists(@"..\..\Xmls\appointments.xml"))
+                {
+                    dataSet.ReadXml(@"..\..\Xmls\appointments.xml");
+                }
+                else
+                {
+                    MessageBox.Show("File does not exist.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+            {
+                if (row["userType"].ToString() == "Admin")
+                {
+                    if(row["email"].ToString() == txtEmail.Text && row["password"].ToString() == txtPass.Text)
+                    {
+                        Dashboard dash = new Dashboard();
+                        this.Hide();
+                        dash.Show();
+                    }
+                    else if(row["email"].ToString() == txtEmail.Text && row["password"].ToString() != txtPass.Text)
+                    {
+                        MessageBox.Show("Please check your details.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sorry, We are unable to recognize you.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ finalproject/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using finalproject.Admin;
+
+namespace finalproject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Admin_Click(object sender, RoutedEventArgs e)
+        {
+            Admin.Admin ad = new Admin.Admin();
+            this.Hide();
+            ad.Show();
+        }
+
+        private void btn_JobSeeker_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
