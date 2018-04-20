a966daee3f7915cd8fa70cd884296c1a41377351
Harjeevan Tanda <hstanda@gmail.com>
2018-04-20 04:27:05 -0400

    added register form

--- finalproject/User/Register.xaml.cs
+++ finalproject/User/Register.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace finalproject.User
 {
@@ -19,9 +22,37 @@ namespace finalproject.User
     /// </summary>
     public partial class Register : Window
     {
+        
+        internal List<Users> usersList = new List<Users>();
+        string USERS_FILE_PATH = @"..\..\Xmls\userTest.xml";
         public Register()
         {
             InitializeComponent();
+            LoadConfig();
+        }
+        public void LoadConfig()
+        {
+            XDocument jobTypes = XDocument.Load(@"..\..\Xmls\jobTypes.xml");
+            JobType.Items.Add("Select Job Type");
+            foreach (XElement element in jobTypes.Descendants("jobType"))
+            {
+                JobType.Items.Add(" " + element.Value);
+            }
+
+            XDocument provinces = XDocument.Load(@"..\..\Xmls\provinces.xml");
+            Province.Items.Add("Select Province");
+            foreach (XElement element in provinces.Descendants("province"))
+            {
+                Province.Items.Add(" " + element.Value);
+            }
+
+            XDocument oldUsers = XDocument.Load(@"..\..\Xmls\user.xml");
+            foreach (XElement element in oldUsers.Element("users").Elements("user"))
+            {
+                Users userObject = new Users(element.Element("userId").Value, element.Element("firstName").Value, element.Element("lastName").Value, element.Element("phone").Value, element.Element("email").Value, element.Element("password").Value, element.Element("address1").Value, element.Element("address2").Value, element.Element("city").Value, element.Element("province").Value, element.Element("zipCode").Value, element.Element("jobType").Value, element.Element("education").Value);
+                usersList.Add(userObject);
+            }
+
         }
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
@@ -43,5 +74,89 @@ namespace finalproject.User
         {
 
         }
+
+        private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void PhoneNumber_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Education_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Address1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Address2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void City_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Province_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void State_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void JobType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void ZipCode_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Users userObject = new Users("4", FirstName.Text, LastName.Text, PhoneNumber.Text, Email.Text, Password.Text, Education.Text, Address1.Text, Address2.Text, City.Text, Province.Text, ZipCode.Text, JobType.Text);
+            usersList.Add(userObject);
+            SaveUsers();
+        }
+        private void SaveUsers()
+        {
+
+            /************
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            settings.IndentChars = "\t";
+            XmlWriter xmlObject = XmlWriter.Create(@"..\..\Xmls\userTest.xml", settings);
+            xmlObject.WriteStartDocument();
+            xmlObject.WriteStartElement("users");
+
+            foreach (var user in usersList)
+            {
+                xmlObject.WriteStartElement("user");
+                xmlObject.WriteElementString("VehicleType", user.FirstName);
+                xmlObject.WriteEndElement();
+            }
+            xmlObject.WriteEndElement();
+            xmlObject.Close();
+            /************/
+        }
+
+
     }
 }
---
+++ finalproject/User/Users.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finalproject.User
+{
+    class Users
+    {
+        private string firstName;
+        private string lastName;
+        private string phoneNumber;
+        private string email;
+        private string password;
+        private string education;
+        private string address1;
+        private string address2;
+        private string city;
+        private string province;
+        private string jobType;
+        private string userId;
+        private string zipCode;
+        
+
+
+        public Users(string userId, string firstName, string lastName, string phoneNumber, string email, string password, string education, string address1, string address2, string city, string province, string zipCode, string jobType)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+            this.password = password;
+            this.education = education;
+            this.address1 = address1;
+            this.address2 = address2;
+            this.city = city;
+            this.province = province;
+            this.jobType = jobType;
+            this.userId = userId;
+        }
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Password { get => password; set => password = value; }
+        public string Email { get => email; set => email = value; }
+        public string Education { get => education; set => education = value; }
+        public string Address1 { get => address1; set => address1 = value; }
+        public string Address2 { get => address2; set => address2 = value; }
+        public string City { get => city; set => city = value; }
+        public string Province { get => province; set => province = value; }
+        public string JobType { get => jobType; set => jobType = value; }
+        public string UserId { get => userId; set => userId = value; }
+        public string ZipCode { get => zipCode; set => zipCode = value; }
+
+    }
+}
